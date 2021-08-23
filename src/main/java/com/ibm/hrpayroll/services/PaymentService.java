1f3ed7a043055bac300b1bb634ab4007eac12099
joaopaulo2000 <joaopauloaraujo2701@gmail.com>
2021-08-23 08:47:25 -0300

    primeiro commit

---
+++ src/main/java/com/ibm/hrpayroll/services/PaymentService.java
@@ -0,0 +1,21 @@
+package com.ibm.hrpayroll.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ibm.hrpayroll.entities.Payment;
+import com.ibm.hrpayroll.entities.Worker;
+import com.ibm.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(Long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
